508c5cde52515b4503440c0ff56dff949d5f77b6
Dax Ravi <d@ironbay.co>
2017-11-07 14:39:45 -0500

    Reorg

--- lib/radar.ex
+++ lib/radar.ex
@@ -1,29 +1,12 @@
 defmodule Radar do
 
-  def join(group) do
-    Registry.register(Radar.Group, group, 1)
-  end
-
-  def broadcast(group, msg) do
-    [Node.self() | Node.list]
-    |> Task.async_stream(&execute_node(&1, fn -> broadcast_local(group, msg) end))
-    |> Enum.to_list
-  end
-
-  defp broadcast_local(group, msg) do
-    Radar.Group
-    |> Registry.dispatch(group, fn entries ->
-      for {pid, _} <- entries, do: GenServer.cast(pid, msg)
-    end)
-  end
-
-  def whereis(name) do
+  def which_node(name) do
     HashRing.Managed.key_to_node(:radar, name)
   end
 
   def execute(name, func) do
     name
-    |> whereis
+    |> which_node
     |> execute_node(func)
   end
 
@@ -40,5 +23,23 @@ defmodule Radar do
           result -> result
         end
     end
+  end
+
+  # Pub Sub
+  def join(group) do
+    Registry.register(Radar.Group, group, 1)
+  end
+
+  def broadcast(group, msg) do
+    [Node.self() | Node.list]
+    |> Task.async_stream(&execute_node(&1, fn -> broadcast_local(group, msg) end))
+    |> Enum.to_list
+  end
+
+  defp broadcast_local(group, msg) do
+    Radar.Group
+    |> Registry.dispatch(group, fn entries ->
+      for {pid, _} <- entries, do: GenServer.cast(pid, msg)
+    end)
   end
 end
